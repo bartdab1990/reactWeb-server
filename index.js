3d860d31865a9a76ae9388a89cd645e4482a50ea
Bart Dabrowski <bart.dabrowski.uk@gmail.com>
2020-01-04 22:32:48 +0000

    change listen connection

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("MongoDB Connected");
-    return server.listen({ port: PORT });
+    return server.connection({ port: PORT });
   })
   .then(res => {
     console.log(`Server running at ${res.url}`);
