f52cd584e5a0b31cebf0ef5b73794ac6e73146c8
Bart Dabrowski <bart.dabrowski.uk@gmail.com>
2020-01-05 22:29:30 +0000

    enable introspection

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
   playground: true,
   context: ({ req }) => ({ req, pubsub })
 });
